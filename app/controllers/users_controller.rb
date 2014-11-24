6228980db7525ad524a5858f845c0e2384b20649
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-24 18:33:50 +0000

    Users can see their badges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-
+    @my_badges = []
+    @my_badges = Badge.where(:user_id => current_user.id)
   end
 
 end
