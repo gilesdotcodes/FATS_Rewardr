8211fc08c5ccef5348a6f3aaf25fe713922960a0
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-24 09:47:45 +0000

    secrets.yml no longer in .gitignore

--- Gemfile
+++ Gemfile
@@ -24,7 +24,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'rails_12factor'
-gem 'heroku_secrets', github: 'alexpeattie/heroku_secrets'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.secret_key_base = ENV['SECRET_KEY_BASE'] if Rails.env == 'production'
+  # config.secret_key_base = ENV['SECRET_KEY_BASE'] if Rails.env == 'production'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
