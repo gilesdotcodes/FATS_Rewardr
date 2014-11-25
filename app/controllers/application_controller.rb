a54f34b5b51b13032b89d36dde230666515d2723
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-25 11:03:44 +0000

    Name added to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
     session["user_return_to"] || users_show_path
   end
 
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      # you control which attributes can be updated or used for sign in here
+    end
+
 end
