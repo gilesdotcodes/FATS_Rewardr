3f800caa1830797c5add6abbd8417facef7dd031
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-26 16:35:52 +0000

    Scroll through the badges before submitting the form. jQuery.

--- app/assets/javascripts/badges.js
+++ app/assets/javascripts/badges.js
@@ -3,15 +3,32 @@
  // You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready(function() {
 
-  var i = 1;
+  var i = 0;
+  var images = ['/images/logo_black-01.png', '/images/padlock.png', '/images/logo_black-01.png', '/images/padlock.png', '/images/logo_black-01.png', '/images/padlock.png'];
+  var reward = ['Award 1', 'Award 2', 'Award 3', 'Award 4', 'Award 5', 'Award 6']
 
-  $('#left').on('click', function(){
+  // $('#left').on('click', function(){
 
-    var images = ['/images/logo_black-01.png', '/images/padlock.png', '/images/logo_black-01.png', '/images/padlock.png', '/images/logo_black-01.png', '/images/padlock.png'];
+  //   $('#reward_image').attr('src', images[i]);
+
+  //   $('#badge_name').val(reward[i])
+
+  //   if(i>0) {
+  //      return i--;
+  //   }
+  //   else {
+  //      return i=5;
+  //   }
+
+  // });
+
+  $('#right').on('click', function(){
 
     $('#reward_image').attr('src', images[i]);
 
-    if(i<6) {
+    $('#badge_name').val(reward[i])
+
+    if(i<5) {
        return i++;
     }
     else {
