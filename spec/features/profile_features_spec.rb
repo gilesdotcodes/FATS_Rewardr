ee3be7e87f6f42f7407f7f64d8dbd0f985c46fce
Andrew Hercules <andrewhercules@gmail.com>
2014-11-26 19:51:31 +0000

    Added additional feature tests and fixed minor CSS issues

--- spec/features/profile_features_spec.rb
+++ spec/features/profile_features_spec.rb
@@ -30,17 +30,27 @@ describe 'Profile page' do
     end
 
     it 'should contain a link back to the personal dashboard of the user' do
-      expect(page).to have_link('Return to Personal Dashboard')
+      expect(page).to have_link('Dashboard')
     end
 
     it 'should contain a link to the Edit Profile form' do
-      expect(page).to have_link('Edit My Profile')
+      expect(page).to have_link('Edit Profile')
+    end
+
+    it 'should allow user to click link to visit Edit Profile form' do
+      click_link('Edit Profile')
+      expect(current_path).to eq('/users/edit')
+    end
+
+    it 'should allow the user to click link to visit Dashboard' do
+      click_link('Dashboard')
+      expect(current_path).to eq('/users/show')
     end
 
     context 'the Recent News section' do
 
       it 'should list badges given to all employees' do
-        click_link 'Return to Personal Dashboard'
+        click_link 'Dashboard'
         click_link 'Sign out'
         award_3_badges
         sign_in('employee@test.com')
@@ -62,7 +72,7 @@ describe 'Profile page' do
     context 'the My Badges Archive section' do
 
       it 'should list all badges user has been given' do
-        click_link 'Return to Personal Dashboard'
+        click_link 'Dashboard'
         click_link 'Sign out'
         award_3_badges
         sign_in('employee@test.com')
@@ -84,31 +94,29 @@ describe 'Profile page' do
     context 'the Edit Profile form' do
 
       it 'should allow user to update his or her name' do
-        click_link 'Edit My Profile'
+        click_link 'Edit Profile'
         fill_in 'Name', with: 'New Employee'
         click_button 'Update'
         expect(page).to have_content('Thank you! Your profile has been successfully updated.')
       end
 
       it 'should allow user to update his or her email address' do
-        click_link 'Edit My Profile'
+        click_link 'Edit Profile'
         fill_in 'Email', with: 'new_employee@test.com'
         click_button 'Update'
         expect(page).to have_content('Thank you! Your profile has been successfully updated.')
       end
 
       it 'should allow user to update his or her password' do
-        click_link 'Edit My Profile'
+        click_link 'Edit Profile'
         fill_in 'Password', with: 'abcd1234'
         fill_in 'Password confirmation', with: 'abcd1234'
         click_button 'Update'
         expect(page).to have_content('Thank you! Your profile has been successfully updated.')
+        expect(current_path).to eq('/users/show')
       end
 
-
     end
-
-
 
   end
 
