7babf22878dc3ddf3348aedf099addff8ebaf27a
Stephen Giles <s.giles@hotmail.co.uk>
2014-12-02 14:26:38 +0000

    Pushing front-end design to Heroku.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def linkedin
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -7,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "linkedin") if is_navigational_format?
     else
-      session["devise.linkedin_data"] = request.env["omniauth.auth"].except("extra") 
+      session["devise.linkedin_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
   end
